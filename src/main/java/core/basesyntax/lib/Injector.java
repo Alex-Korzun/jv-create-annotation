be8ac5484101c0dc573155ce166045c3efdd1d55
Alex Korzun <koleksandr4@gmail.com>
2021-01-11 13:53:05 +0200

    added requested changes

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -1,7 +1,9 @@
 package core.basesyntax.lib;
 
 import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.BetDaoImpl;
 import core.basesyntax.dao.GamblerDao;
+import core.basesyntax.dao.GamblerDaoImpl;
 import core.basesyntax.exception.NoSuchAnnotationException;
 import core.basesyntax.factory.Factory;
 import java.lang.reflect.Constructor;
@@ -14,15 +16,19 @@ public class Injector {
         Constructor declaredConstructor = clazz.getDeclaredConstructor();
         Object instance = declaredConstructor.newInstance();
         Field[] declaredFields = clazz.getDeclaredFields();
+
         for (Field field : declaredFields) {
             if (field.getAnnotation(Inject.class) != null) {
                 field.setAccessible(true);
-                if (field.getType().equals(BetDao.class)) {
+                if (field.getType().equals(BetDao.class)
+                        && BetDaoImpl.class.isAnnotationPresent(Dao.class)) {
                     field.set(instance, Factory.getBetDao());
-                } else if (field.getType().equals(GamblerDao.class)) {
+                } else if (field.getType().equals(GamblerDao.class)
+                        && GamblerDaoImpl.class.isAnnotationPresent(Dao.class)) {
                     field.set(instance, Factory.getGamblerDao());
                 } else {
-                    throw new NoSuchAnnotationException("There is no such annotation!");
+                    throw new NoSuchAnnotationException("There is no " + Dao.class + " annotation"
+                                                + " in " + clazz.getName());
                 }
             }
         }
